77b47254638ec98af5ded111ff19b471c5649809
Muhammad Rasyid Khaikal <mrasyid.haikal@gmail.com>
2020-05-11 10:56:09 +0700

    Initial Commit

---
+++ Bed Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bed Time
+//
+//  Created by Muhammad Rasyid khaikal on 11/05/20.
+//  Copyright © 2020 Muhammad Rasyid khaikal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
